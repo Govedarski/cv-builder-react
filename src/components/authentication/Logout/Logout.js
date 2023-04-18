3725de824589e1396bece9fed81078158dce558e
Govedarski <velislavgovedarski@gmail.com>
2023-04-18 23:32:01 +0300

    Fix logout bug

--- src/components/authentication/Logout/Logout.js
+++ src/components/authentication/Logout/Logout.js
@@ -1,14 +1,18 @@
 import {useContext} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import {UserContext} from '../../../context/UserContext.js';
 import {routes} from "../../../constants/routes";
 
 
 export function Logout() {
     const {userLogout} = useContext(UserContext);
+    const location = useLocation();
 
     function handleLogout(userLogout) {
         userLogout();
+        if (location.pathname === routes.HOME) {
+            window.location.reload();
+        }
     }
 
     return (
